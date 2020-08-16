ce070b9889398516a73a8fa6ea0129922c4200ba
Kelechi-Eluagu <kellyeluagu@gmail.com>
2020-08-16 21:02:50 +0100

    work in progress

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
-var onloadCallback = function() {
-      alert("grecaptcha is ready!");
-    };
+// var onloadCallback = function() {
+//       alert("grecaptcha is ready!");
+//     };
+
+function validate() {
+  let errorEmail = getElementById('error-email');
+  let email = getElementById('email');
+  let phone = getElementById('phone');
+  let firstName = getElementById('firstName');
+  let lastName = getElementById('lastName');
+  let message;
+
+  if(email.indexOf('@') == -1 || email.length < 4 || email == '') {
+    // message = 'This field is required';
+    // errorEmail.innerHTML = message;
+    // return false;
+    console.log('name grabbed');
+
+  }
+}
