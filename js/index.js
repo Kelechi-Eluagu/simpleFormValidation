38dcf8da2ba3246109d1cc53b506a3b82a5e4aac
Kelechi-Eluagu <kellyeluagu@gmail.com>
2020-08-16 22:50:48 +0100

    implementing js

--- js/index.js
+++ js/index.js
@@ -3,20 +3,28 @@
 //     };
 
 function validate() {
-  let email = document.getElementById('email');
-  console.log('grabbed');
-  // let phone = document.getElementById('phone');
-  // let firstName = document.getElementById('firstName');
-  // let lastName = document.getElementById('lastName');
+  let email = document.getElementById('email').value;
+  let phone = document.getElementById('phone').value;
+  let firstName = document.getElementById('firstName').value;
+  let lastName = document.getElementById('lastName').value;
+  let errorEmail = document.getElementById('error-email');
   // let errorEmail = document.getElementById('error-email');
-  // let message;
+  let errorFirst = document.getElementById('error-first');
+  let message;
+
+  if(firstName === '' || firstName.length < 4) {
+    message = 'This field is required';
+    errorFirst.innerHTML = message; 
+    return false;
+  }
 
   // if(email.indexOf('@') == -1 || email.length < 4 || email == '') {
   //   message = 'This field is required';
   //   errorEmail.innerHTML = message;
   //   return false;
-  //   // console.log('name grabbed');
+  //   console.log('name grabbed');
   // }
-  // alert('Form Submitted Successfully');
+  alert('Form Submitted Successfully');
 
+  return true;
 }
